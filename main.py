e7851e10d66568aab44f8c9a60a56d9679cbb5ae
Nathaniel Worcester <nathaniel.worcester@gmail.com>
2018-10-20 21:55:55 -0500

    added caesar.py and finished web caesar

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, redirect, render_template
 import cgi
 import os
-
+from caesar import rotate_string
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -10,3 +10,17 @@ app.config['DEBUG'] = True
 @app.route('/')
 def index():
     return render_template('caesar-form.html')
+
+@app.route('/', methods = ['POST'])
+
+def encrypt():
+    rotated_string=''
+    rot = request.form['rot']
+    text = request.form['text']
+
+    rot = int(rot)
+
+    rotated_string = rotate_string(text, rot)
+
+    return render_template('caesar-form.html', rotated_string=rotated_string)
+app.run()
